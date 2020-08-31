2a1a48e048a95cb62475e5da98423ecae192e9b2
AlveeM <alvee.akand@outlook.com>
2020-08-31 08:44:57 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ text: '' });
+    this.props.addRestaurant(this.state);
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Restaurant Name</label>
+          <input type="text" name="name" value={this.state.text} onChange={this.handleChange}></input>
+          <input type="submit" value="Submit"></input>
+        </form>
       </div>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
+import { connect } from 'react-redux'; 
 
 class ReviewsContainer extends Component {
-
   render() {
+    const { restaurant } = this.props;
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput restaurantId={restaurant.id} addReview={this.props.addReview} />
+        <Reviews reviews={this.props.reviews} deleteReview={this.props.deleteReview} restaurantId={restaurant.id} />
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addReview: (review) => dispatch({ type: 'ADD_REVIEW', review }),
+    deleteReview: (id) => dispatch({ type: 'DELETE_REVIEW', id }),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
